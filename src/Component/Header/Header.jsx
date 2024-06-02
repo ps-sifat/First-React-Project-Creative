7ba655abc96a8dbc89da00eb2abdd15daad29b0f
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-06-03 01:25:31 +0600

    Navbar done

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/navLogo.png";
+import "./Header.css";
 
 const Header = () => {
+  const navItem = ["Home", "About", "Service", "Blog", "Contact"];
   return (
     <>
-    <div className="container">
-        
-    </div>
+      <div className="container">
+        <div className="mainNav">
+          <div className="navLogo">
+            <img className="logo" src={logo} alt={logo} />
+          </div>
+          <div className="navMenu">
+            <ul className="navMenu__main">
+              {navItem.map((item) => (
+                <li>
+                  <a className="navList" href="#">
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="navBtn">
+            <a className="btn" href="#">
+              Contact us
+            </a>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Header;
