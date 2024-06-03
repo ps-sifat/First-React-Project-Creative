c50b23ba2d3b7bfdf56e8406994e08bc2e335ab8
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-06-04 00:15:49 +0600

    service part add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Component/Header/Header'
 import Banner from './Component/Banner/Banner'
+import Service from './Component/Service/Service'
 const App = () => {
   return (
     <div>
       <Header />
       <Banner />
+      <Service />
     </div>
 
   )
---
+++ src/Component/Service/Service.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Service = () => {
+  return (
+    <>
+      <div className="servicePart">
+        <h3>Our Service</h3>
+        
+      </div>
+    </>
+  );
+};
+
+export default Service;
