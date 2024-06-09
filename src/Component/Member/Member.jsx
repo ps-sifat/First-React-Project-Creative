14d4f0c38e8e9ba9b0387fd5291866782a53a4eb
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-06-09 23:54:40 +0600

    member layout

--- src/Component/Member/Member.jsx
+++ src/Component/Member/Member.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "./Member.css";
+import member01 from "../../assets/member01.png";
+import member02 from "../../assets/member02.png";
+import member03 from "../../assets/member03.png";
 
 const Member = () => {
+  const allmember = [
+    {
+      id: 1,
+      memberImg: member01,
+      name: "Mahid Ahmed",
+      des: "UX Designer",
+    },
+    {
+      id: 2,
+      memberImg: member02,
+      name: "Hasan Imam",
+      des: "UI Designer",
+    },
+    {
+      id: 3,
+      memberImg: member03,
+      name: "Kazi Afia",
+      des: "Digital Marketor",
+    },
+  ];
   return (
     <>
       <div id="memberPart">
@@ -17,9 +40,17 @@ const Member = () => {
         {/* mermber part */}
         <div className="container">
           <div className="memberWarrper">
-            <picture>
-              <img src="" alt="" />
-            </picture>
+            {allmember?.map((item) => (
+              <div className="member" key={item.id}>
+                <picture>
+                  <img src={item.memberImg} alt={member01} />
+                </picture>
+                <div className="memberOverlay">
+                  <h4 className="memberOverlay__title">{item.name}</h4>
+                  <p className="memberOverlay__details">{item.des}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
