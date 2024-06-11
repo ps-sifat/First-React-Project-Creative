0a78f48956797ef96006fedd9cf84eb1db10bdaa
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-06-11 23:51:14 +0600

    customer

--- src/Component/Customer/Customer.jsx
+++ src/Component/Customer/Customer.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import "./Customer.css";
 
 const Customer = () => {
   return (
     <>
       <div id="customerPart">
-        <div className="customerPart__title">
-          <div className="customerPart__header">
-            Wer Care About Our Customer Experience Too
-          </div>
-          <div className="customerPart__details">
-            There are many variations of passages of Lorem Ipsum available, but
-            the majority have suffered alteration in some form.
+        <div className="container">
+          <div className="customerPart__title">
+            <div className="customerPart__header">
+              Wer Care About Our Customer Experience Too
+            </div>
+            <div className="customerPart__details">
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form.
+            </div>
           </div>
         </div>
       </div>
