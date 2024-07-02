3b567429e09bf2eea91702d95d1ce0be4da39ac9
sifatul-islam-bd <mdsifatulislam2357@gmail.com>
2024-07-02 23:48:11 +0600

    someupdate

--- src/Component/Customer/Customer.jsx
+++ src/Component/Customer/Customer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Customer.css";
 import clintImg1 from "../../assets/customer.png";
+import { IoIosStar } from "react-icons/io";
+
 const Customer = () => {
+  const allCustomer = [
+    {
+      id: 1,
+      customerImg: <clintImg1 />,
+      title: "UI/UX Design",
+      description: `There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form.`,
+    },
+  ];
   return (
     <>
       <div id="customerPart">
@@ -20,6 +31,21 @@ const Customer = () => {
               <picture>
                 <img src={clintImg1} alt={clintImg1} />
               </picture>
+              <div className="customer__icon">
+                <IoIosStar />
+                <IoIosStar />
+                <IoIosStar />
+                <IoIosStar />
+                <IoIosStar />
+              </div>
+            </div>
+            <p className="customer__details">
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form.
+            </p>
+            <div className="customer__clint">
+              <h3>Mahid Ahmed</h3>
+              <p>UX Designer</p>
             </div>
           </div>
         </div>
